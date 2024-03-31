a106b38d2d63e5e4e327fd57edc403b2d5461098
Hà Anh Thảo <81128952+iuh20001575@users.noreply.github.com>
2024-03-31 09:18:12 +0700

    Update start.js

--- start.js
+++ start.js
@@ -5,13 +5,13 @@ const { query } = require("./data.js");
 
 app.use(express.json());
 
-app.get("/api/books", async (req, res) => {
+app.get("/api20001575/books", async (req, res) => {
   console.info(`Getting all books`);
 
-  const books = await query("SELECT * FROM tblbook");
+  const books = await query("SELECT * FROM tblbookIUH20001575");
   res.json(books);
 });
 
-app.listen(80, () => {
+app.listen(5000, () => {
   console.info(`Server started listening on port 80`);
 });
